9140ef0e7282756fdab2a39332de4555bf4cf28d
Griffin Solot-Kehl <griffin@transposit.com>
2019-08-06 15:07:23 +0000

    Commit from Transposit IDE

--- src/webhook.js
+++ src/webhook.js
@@ -3,13 +3,13 @@
   let content = api.run('this.html')[0];
   let email = body.Body.trim().toLowerCase();
   
-  //Code to ignore duplicate entries
-  let phones = stash.get("phones");
-  if (phones == null) {
-    stash.put("phones", [body.From]);
+  // Code to ignore duplicate entries
+  let phones = [];
+  if (stash.get("phones") == null) {
+    stash.put("phones", phones.push(body.From));
     api.run('this.create_record', {email: email, baseid: env.get("baseid")});
   } else {
-    if (!phones.includes(body.From)) {
+    if (!stash.get("phones").includes(body.From)) {
       stash.put("phones", phones.push(body.From));
       api.run('this.create_record', {email: email, baseid: env.get("baseid")});
     }
